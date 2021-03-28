f523c29da7a684e8344528d7e835523e4af36c58
sumiship <osumi.k.osumi.k@gmail.com>
2021-03-28 14:26:35 +0900

    gameSystemComplete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 "use script";
 
 {
+  let score = 0;
+  let stage = 0;
+  const score_view = document.getElementById("score_view");
+  const stage_view = document.getElementById("stage");
   const ctx = document.getElementById("canvas").getContext("2d");
 
   ctx.beginPath();
@@ -69,6 +73,30 @@
       } else {
         return null;
       }
+    }
+    crossArc(point) {
+      let A = this.begin;
+      let B = this.end;
+      let P = point;
+      if (
+        (A.y - 3 <= P.y && P.y <= B.y + 3) ||
+        (B.y - 3 <= P.y && P.y <= A.y + 3)
+      ) {
+        if (
+          (A.x - 3 <= P.x && P.x <= B.x + 3) ||
+          (B.x - 3 <= P.x && P.x <= A.x + 3)
+        ) {
+          if (
+            Math.abs(
+              P.y * (A.x - B.x) + A.y * (B.x - P.x) + B.y * (P.x - A.x)
+            ) < 1000
+          ) {
+            return true;
+          }
+        }
+      }
+      // 点Pが線分AB上にない
+      return false;
     }
   }
 
@@ -145,7 +173,7 @@
       this.level = new Level();
     }
     reset() {
-      this.player.pos = new Vec2(118, 201);
+      this.player.pos = new Vec2(135, 330);
       this.player.angle = -Math.PI / 2;
     }
   }
@@ -163,11 +191,11 @@
         "..OOO..." +
         "..O....." +
         "........" +
-        "........" +
-        "........" +
-        "......O." +
-        "OO...OO." +
-        "OO...O..",
+        "..O....." +
+        ".O.O...." +
+        "...O...." +
+        "OOOO..O." +
+        "......O.",
       8,
       10,
       35
@@ -176,11 +204,7 @@
   }
   setup();
 
-  let player;
-
-  player = new Player();
-  player.pos = new Vec2(100, 200);
-  player.angle = Math.PI / 1;
+  let player = game.player;
 
   function line(a, b) {
     ctx.beginPath();
@@ -194,14 +218,6 @@
     ctx.arc(a.x, a.y, 5, 0, Math.PI * 2, false);
     ctx.fill();
   }
-
-  function onClick(e) {
-    var rect = e.target.getBoundingClientRect();
-    player.pos.x = e.clientX - rect.left;
-    player.pos.y = e.clientY - rect.top;
-    draw();
-  }
-  canvas.addEventListener("click", onClick, false);
 
   let walls = game.level.walls;
 
@@ -222,7 +238,7 @@
     angle = player.angle - Math.PI / 4;
     for (let beam of beams) {
       // 交点を求める
-
+      beam.way = beam.way.mult(3);
       let allHitBeamWays = walls
         .map((wall) => beam.intersection(wall))
         .filter((pos) => pos !== null)
@@ -238,20 +254,92 @@
       let wallPerpDist =
         wallDist * Math.cos(angle + (Math.PI / 205) * index - player.angle);
       let lineHeight = 15000 / wallPerpDist;
-      let viewRoot = new Vec2(1.5, 250);
+      let viewRoot = new Vec2(1.5, 240);
       let lineBegin = viewRoot.add(new Vec2(3 * index, -lineHeight / 2));
-      let lineEnd = lineBegin.add(new Vec2(0, lineHeight));
+      // let lineEnd = lineBegin.add(new Vec2(0, lineHeight));
       ctx.fillRect(lineBegin.x - 1.5, lineBegin.y, 3, lineHeight);
       index++;
     }
   }
+
+  class Item {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.time = 0;
+    }
+    static withBox(boxX, boxY) {
+      return new Item(boxX * 35 - 17.5, boxY * 35 - 17.5);
+    }
+    catch(a) {
+      if ((this.x - a.x) ** 2 + (this.y - a.y) ** 2 < 144) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  // let items = [Item.withBox(1, 1), Item.withBox(8, 10)];
+
+  function createItem() {
+    let randomX = Math.floor(Math.random() * 8) + 1;
+    let randomY = Math.floor(Math.random() * 10) + 1;
+    if (
+      (randomX == 1 && randomY == 9) ||
+      (randomX == 2 && randomY == 9) ||
+      (randomX == 2 && randomY == 7) ||
+      (randomX == 3 && randomY == 3) ||
+      (randomX == 3 && randomY == 3) ||
+      (randomX == 3 && randomY == 6) ||
+      (randomX == 3 && randomY == 9) ||
+      (randomX == 4 && randomY == 3) ||
+      (randomX == 4 && randomY == 7) ||
+      (randomX == 4 && randomY == 8) ||
+      (randomX == 4 && randomY == 9) ||
+      (randomX == 5 && randomY == 3) ||
+      (randomX == 7 && randomY == 9) ||
+      (randomX == 7 && randomY == 10)
+    ) {
+      createItem();
+    } else {
+      for (let item of items) {
+        if (item.boxX == randomX && item.boxY == randomY) {
+          createItem();
+          return;
+        }
+      }
+      items.push(Item.withBox(randomX, randomY));
+    }
+  }
+
+  function removeItem(a) {
+    for (let index in items) {
+      if (items[index].catch(a)) {
+        items.splice(index, 1);
+        score++;
+        score_view.innerHTML = score;
+      }
+    }
+  }
+
   function drawMap() {
     ctx.clearRect(0, 0, 85, 105);
     for (let wall of walls) {
       line(wall.begin.mult(0.3), wall.end.mult(0.3));
     }
+    for (let item of items) {
+      let r = 15 + item.time * 8;
+      let b = 255 - item.time * 8;
+      ctx.fillStyle = "rgb(" + r + ",0," + b + ")";
+      ctx.beginPath();
+      ctx.arc(item.x * 0.3, item.y * 0.3, 12 * 0.3, 0, Math.PI * 2, false);
+      ctx.fill();
+      ctx.fillStyle = "rgb(0,0,0)";
+    }
     point(player.pos.mult(0.3));
     for (let beam of beams) {
+      beam.way = beam.way.mult(0.2);
       ctx.strokeStyle = "rgba(255,255,0,0.6)";
       line(beam.begin.mult(0.3), beam.end.mult(0.3));
       ctx.strokeStyle = "rgba(0,0,0,1)";
@@ -273,48 +361,114 @@
     drawMap();
   }
 
+  function moveValidation(a, action) {
+    let test;
+    switch (action) {
+      case "forward":
+        test = a.pos.add(
+          new Vec2(Math.cos(a.angle), Math.sin(a.angle)).mult(5)
+        );
+        break;
+      case "back":
+        test = a.pos.sub(
+          new Vec2(Math.cos(a.angle), Math.sin(a.angle)).mult(5)
+        );
+        break;
+      case "right":
+        test = a.pos.add(
+          new Vec2(
+            Math.cos(a.angle + Math.PI / 2),
+            Math.sin(a.angle + Math.PI / 2)
+          ).mult(5)
+        );
+        break;
+      case "left":
+        test = a.pos.add(
+          new Vec2(
+            Math.cos(a.angle - Math.PI / 2),
+            Math.sin(a.angle - Math.PI / 2)
+          ).mult(5)
+        );
+    }
+    let flag = true;
+    for (let wall of walls) {
+      if (wall.crossArc(test)) {
+        flag = false;
+      }
+    }
+    if (flag) {
+      a.pos = test;
+    }
+    removeItem(a.pos);
+    draw();
+  }
+
+  let key_ready = false;
+
   addEventListener("keydown", (event) => {
-    console.log(event.keyCode);
     key_code = event.keyCode;
-    if (key_code === 67) {
+    if (key_code === 67 && key_ready) {
       player.angle += Math.PI / 16;
       draw();
     }
-    if (key_code === 90) {
+    if (key_code === 90 && key_ready) {
       player.angle -= Math.PI / 16;
       draw();
     }
-    if (key_code === 38) {
-      player.pos = player.pos.add(
-        new Vec2(Math.cos(player.angle), Math.sin(player.angle)).mult(5)
-      );
-      draw();
+    if (key_code === 38 && key_ready) {
+      moveValidation(player, "forward");
     }
-    if (key_code === 40) {
-      player.pos = player.pos.sub(
-        new Vec2(Math.cos(player.angle), Math.sin(player.angle)).mult(5)
-      );
-      draw();
+    if (key_code === 40 && key_ready) {
+      moveValidation(player, "back");
     }
-    if (key_code === 39) {
-      player.pos = player.pos.add(
-        new Vec2(
-          Math.cos(player.angle + Math.PI / 2),
-          Math.sin(player.angle + Math.PI / 2)
-        ).mult(5)
-      );
-      draw();
+    if (key_code === 39 && key_ready) {
+      moveValidation(player, "right");
     }
-    if (key_code === 37) {
-      player.pos = player.pos.add(
-        new Vec2(
-          Math.cos(player.angle - Math.PI / 2),
-          Math.sin(player.angle - Math.PI / 2)
-        ).mult(5)
-      );
-      draw();
+    if (key_code === 37 && key_ready) {
+      moveValidation(player, "left");
     }
   });
 
+  let loop = 0;
+  let speed = 0;
+  let fire = false;
+  function bomb() {
+    if (loop % 24 == 0 && speed < 5) {
+      speed++;
+      stage++;
+      stage_view.innerHTML = stage;
+    }
+    for (let item of items) {
+      if (item.time++ >= 30) {
+        fire = true;
+      }
+    }
+    if (loop % (8 - speed) == 0) createItem();
+    loop++;
+    console.log(loop);
+    draw();
+    var id = setTimeout(bomb, 1000);
+    if (loop > 10000 || fire) {
+      clearTimeout(id);
+      btnStart.innerHTML = "Restart";
+      btnStart.disabled = false;
+      btnStart.style.opacity = "1";
+      items = [Item.withBox(1, 1), Item.withBox(8, 10)];
+      loop = 0;
+      stage = 0;
+      fire = false;
+      key_ready = false;
+      game.reset();
+    }
+  }
+  let items = [Item.withBox(1, 1), Item.withBox(8, 10)];
   draw();
+  const btnStart = document.getElementById("start");
+  btnStart.addEventListener("click", () => {
+    btnStart.disabled = true;
+    btnStart.style.opacity = "0.4";
+    key_ready = true;
+    draw();
+    bomb();
+  });
 }
